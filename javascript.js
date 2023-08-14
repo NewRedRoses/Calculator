baa214116b655c7029d0bee6464902e8c0a7de05
Mark A <mark001616@gmail.com>
2023-08-14 19:28:44 -0400

    Add elementary arithmetic functions

--- javascript.js
+++ javascript.js
@@ -1,1 +1,34 @@
-console.log("test");
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(numberA, operator, numberB) {
+  switch (operator) {
+    case "+":
+      console.log(add(numberA, numberB));
+      break;
+    case "-":
+      console.log(subtract(numberA, numberB));
+      break;
+    case "*":
+      console.log(multiply(numberA, numberB));
+      break;
+    case "/":
+      console.log(divide(numberA, numberB));
+      break;
+  }
+}
+
+operate(1, "/", 2);
